2a3c6d5ecb6d3442cf15cbf3a291a7b1c5621d54
ditateum <ditateum@gmail.com>
2023-04-28 22:25:49 +0700

    Adding update and delete operations

--- lib/src/data/firestore_repository.dart
+++ lib/src/data/firestore_repository.dart
@@ -1,21 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:faker_app_flutter_firebase/src/data/job.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FirestoreRepository {
   FirestoreRepository(this._firestore);
   final FirebaseFirestore _firestore;
 
-  Future<void> addJob(String uid, String title, String company) async {
-    final docRef = await _firestore.collection('jobs').add({
-      'uid': uid,
-      'title': title,
-      'company': company,
-    });
+  Future<void> addJob(String uid, String title, String company) =>
+      _firestore.collection('jobs').add({
+        'uid': uid,
+        'title': title,
+        'company': company,
+      });
 
-    debugPrint(docRef.id);
-  }
+  /// doc('jobs/$jobId') same with collection('jobs').doc(jobId)
+  Future<void> updateJob(
+          String uid, String jobId, String title, String company) =>
+      _firestore.doc('jobs/$jobId').update({
+        'uid': uid,
+        'title': title,
+        'company': company,
+      });
+
+  /// doc('jobs/$jobId') same with collection('jobs').doc(jobId)
+  Future<void> deleteJob(String uid, String jobId) =>
+      _firestore.collection('jobs').doc(jobId).delete();
 
   Query<Job> jobsQuery() {
     return _firestore.collection('jobs').withConverter(
